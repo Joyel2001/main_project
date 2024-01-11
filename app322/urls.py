58bdbc76997ad7cb3d2c67f8978d23ddeb73e826
joyel <joyeljoy2024@mca.ajce.in>
2024-01-11 15:59:23 +0530

    done

--- app322/urls.py
+++ app322/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from .views import all_posts,post_detail
 urlpatterns = [
     path('post_content/', views.post_content, name='post_content'),
+    path('all_posts/', all_posts, name='all_posts'),
+    path('post_detail/<int:post_id>/', post_detail, name='post_detail'),
 ]
--- app322/views.py
+++ app322/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate,login,logout
+from django.views.decorators.cache import never_cache
 # Create your views here.
 #  cumunity forum post section 
 # main/views.py
 from django.shortcuts import render, redirect
 from .models import Post
-
+# @login_required(login_url='loginn')
 def post_content(request):
     if request.method == 'POST':
         title = request.POST.get('title')
@@ -22,5 +23,31 @@ def post_content(request):
 
     return render(request, 'main/forum/newpost.html')
   # Render the form page for GET requests
+
+
+
+# display post
+
+
+# main/views.py
+# views.py
+from django.shortcuts import render
+from .models import Post
+
+def all_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'main/forum/post_detail.html', {'posts': posts})
+
+
+# elaborated post details
+# views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'main/forum/elaborated.html', {'post': post})
+
+
 
 
